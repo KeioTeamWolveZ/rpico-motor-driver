df742df3e72880927fc87f2a557978297e05838c
sdnk-rk <r_sadanaka@keio.jp>
2026-06-29 13:10:27 +0900

    interactive dubug

--- rpico-motor-driver.cpp
+++ rpico-motor-driver.cpp
@@ -17,16 +17,27 @@ Motor motor[2] = {Motor(pwm[3], pwm[0], enc[0]), Motor(pwm[1], pwm[2], enc[1])};
 
 char buf[255];
 
-void readline(char* buf) {
+bool readline(char* buf, int buf_size) {
     int i = 0;
     while (1) {
         char c = getchar();
         if (c == '\n') {
             break;
         }
+        if (c == '\r') {
+            continue;
+        }
+        if (i >= buf_size - 1) {
+            buf[buf_size - 1] = 0;
+            while (c != '\n') {
+                c = getchar();
+            }
+            return false;
+        }
         buf[i++] = c;
     }
     buf[i] = 0;
+    return true;
 }
 
 bool timer_cb(repeating_timer_t* rt) {
@@ -49,7 +60,7 @@ void initTimer() {
 }
 
 void setup() {
-    stdio_init_all();
+    printf("DBG setup start\n");
     gpio_set_dir(26, GPIO_IN);
     gpio_set_dir(27, GPIO_IN);
     gpio_set_dir(28, GPIO_IN);
@@ -64,38 +75,66 @@ void setup() {
 
     servo.init();
     initTimer();
+    printf("DBG setup done\n");
 }
 
 int main() {
+    stdio_init_all();
+    printf("DBG boot\n");
     setup();
+    printf("DBG loop start\n");
     while (true) {
-        readline(buf);
-        int id;
-        int mode;
-        double val;
-        sscanf(buf, "%d %d %lf", &id, &mode, &val);
-        printf("id: %d mode: %d val: %f\n", id, mode, val);
+        if (!readline(buf, sizeof(buf))) {
+            printf("ERR line_too_long\n");
+            continue;
+        }
+        printf("DBG rx raw=\"%s\"\n", buf);
+
+        int id = 0;
+        int mode = 0;
+        double val = 0.0;
+        int parsed = sscanf(buf, "%d %d %lf", &id, &mode, &val);
+        if (parsed != 3) {
+            printf("ERR parse raw=\"%s\"\n", buf);
+            continue;
+        }
+        printf("DBG parsed id=%d mode=%d val=%.3f\n", id, mode, val);
+
+        if (id < 0 || id > 2) {
+            printf("ERR id out_of_range id=%d\n", id);
+            continue;
+        }
+        if ((id == 0 || id == 1) && mode != 0 && mode != 1) {
+            printf("ERR mode invalid id=%d mode=%d\n", id, mode);
+            continue;
+        }
+
         switch (id) {
             case 0:
                 if (!mode) {
                     motor[0].disablePosPid();
                     motor[0].setVel(val);
+                    printf("OK motor id=0 mode=vel target=%.3f\n", val);
                 } else {
                     motor[0].resetPos();
                     motor[0].setPos(val);
+                    printf("OK motor id=0 mode=pos target=%.3f\n", val);
                 }
                 break;
             case 1:
                 if (!mode) {
                     motor[1].disablePosPid();
                     motor[1].setVel(val);
+                    printf("OK motor id=1 mode=vel target=%.3f\n", val);
                 } else {
                     motor[1].resetPos();
                     motor[1].setPos(val);
+                    printf("OK motor id=1 mode=pos target=%.3f\n", val);
                 }
                 break;
             case 2:
                 servo.write((int)val);
+                printf("OK servo id=2 mode=%d angle=%d\n", mode, (int)val);
                 break;
         }
     }
